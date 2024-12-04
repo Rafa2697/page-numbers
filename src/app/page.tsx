72a4b6d8068f8d264444170b15fd73b10f174076
Rafael <Rafael.mattos468@gmail.com>
2024-12-04 19:03:06 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card"
+
+export default function Home() {
+  return (
+    <HoverCard>
+      <HoverCardTrigger>Hover</HoverCardTrigger>
+      <HoverCardContent>
+        The React Framework – created and maintained by @vercel.
+      </HoverCardContent>
+    </HoverCard>
+
+  );
+}
