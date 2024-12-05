3ae1df5c423bf5390d3045ea0bb153778a461296
Rafael <Rafael.mattos468@gmail.com>
2024-12-04 21:45:59 -0300

    Feat: Aqui já está implementado a princial funcionalidade do projeto, ainda falta ajustar layout e ui/ux.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,44 @@
-import Image from "next/image";
-import {
-  HoverCard,
-  HoverCardContent,
-  HoverCardTrigger,
-} from "@/components/ui/hover-card"
+'use client'
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card";
+import numeros from "@/data/numeros";
+import { useState, ReactNode } from "react";
 
+const Item = ({ numero }: {
+  numero: {
+    numero: ReactNode; string: string; ativo: boolean; local: string;
+  }
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  return (
+    <div className="bg-slate-800 p-2 rounded-lg flex justify-center mt-2 hover:bg-gray-900 shadow-md">
+      <HoverCard open={isOpen} onOpenChange={setIsOpen} openDelay={2}>
+        <HoverCardTrigger onClick={handleToggle} className="text-white cursor-pointer ">
+          {numero.numero}
+        </HoverCardTrigger>
+        <HoverCardContent >
+          <p>Número: {numero.numero}</p>
+          <p>Contato: {numero.local}</p>
+          <p className={`${numero.ativo ? "text-green-500" : "text-red-500"}`}>WhatsApp: {numero.ativo ? "Ativo" : "Inativo"}</p>
+        </HoverCardContent>
+      </HoverCard>
+    </div>
+  );
+};
 export default function Home() {
   return (
-    <HoverCard>
-      <HoverCardTrigger>Hover</HoverCardTrigger>
-      <HoverCardContent>
-        The React Framework – created and maintained by @vercel.
-      </HoverCardContent>
-    </HoverCard>
-
+    <div className="bg-slate-400 w-40 md:w-1/2 h-4/5 flex items-center justify-center rounded-lg shadow-xl">
+      <ul className="flex flex-col justify-evenly h-full">
+        {numeros.map((numero, index) => (
+          <li key={index}>
+            <Item numero={{ ...numero, string: '' }} />
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
---
+++ src/data/numeros.ts
@@ -0,0 +1,12 @@
+const numeros = [
+    { numero: "13 3456-2979", ativo: true,  local:"Secretaria Peruíbe" },
+    { numero: "13 3456-3055", ativo: false, local:"Secretaria Peruíbe" },
+    { numero: "13 3426-2390", ativo: true, local:"Secretaria Itanhaém" },
+    { numero: "13 99178-4167", ativo: true,local:"Vestibular" },
+    { numero: "13 97816-9063", ativo: false, local:"Vestibular" },
+    { numero: "13 97413-8167", ativo: false, local:"Vestibular" },
+    { numero: "13 99156-9514", ativo: true, local:"Vestibular" },
+  ];
+  
+  export default numeros;
+  
