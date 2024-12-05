c0d95f2a1352a490df3aeea5c96ba0d80c419e7f
Rafael <Rafael.mattos468@gmail.com>
2024-12-05 17:05:47 -0300

    Feat: colcoando o numero da univr

--- src/data/numeros.ts
+++ src/data/numeros.ts
@@ -3,9 +3,10 @@ const numeros = [
   { numero: "13 3456-3055", ativo: false, local: "Secretaria Peruíbe", imag: "https://portal.unisepe.com.br/fpbe/wp-content/uploads/sites/10008/2024/12/logoFPBE.png" },
   { numero: "13 3426-2390", ativo: true, local: "Secretaria Itanhaém", imag: "https://portal.unisepe.com.br/fpbe/wp-content/uploads/sites/10008/2024/12/logoFASUPI.png" },
   { numero: "13 99178-4167", ativo: true, local: "Vestibular", imag: "https://portal.unisepe.com.br/fpbe/wp-content/uploads/sites/10008/2024/12/logoUNISEPE.png" },
-  { numero: "13 97816-9063", ativo: false, local: "Vestibular", imag: "https://portal.unisepe.com.br/fpbe/wp-content/uploads/sites/10008/2024/12/logoUNISEPE.png" },
-  { numero: "13 97413-8167", ativo: false, local: "Vestibular", imag: "https://portal.unisepe.com.br/fpbe/wp-content/uploads/sites/10008/2024/12/logoUNISEPE.png" },
+  { numero: "13 97816-9063", ativo: true, local: "Vestibular", imag: "https://portal.unisepe.com.br/fpbe/wp-content/uploads/sites/10008/2024/12/logoUNISEPE.png" },
+  { numero: "13 97413-8167", ativo: true, local: "Vestibular", imag: "https://portal.unisepe.com.br/fpbe/wp-content/uploads/sites/10008/2024/12/logoUNISEPE.png" },
   { numero: "13 99156-9514", ativo: true, local: "Vestibular", imag: "https://portal.unisepe.com.br/fpbe/wp-content/uploads/sites/10008/2024/12/logoUNISEPE.png" },
+  { numero: "13 3828-2840", ativo: true, local: "Secretaria UNIVR", imag: "https://portal.unisepe.com.br/fpbe/wp-content/uploads/sites/10008/2024/12/Design-sem-nome-9.png" },
 ];
 
 export default numeros;
